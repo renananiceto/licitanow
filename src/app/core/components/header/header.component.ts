0615399b19b20313a0226bf9b0e6ce46e4af5a69
renananiceto <renan.a.ferreira94@gmail.com>
2026-02-26 03:01:36 +0000

    construindo page home

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,9 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { ButtonComponent } from '../../../shared/components/button/button.component';
+
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
+  imports: [ButtonComponent],
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+
 })
 export class HeaderComponent { }
---
+++ src/app/shared/components/button/button.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss']
+})
+export class ButtonComponent {
+  @Input() label: string = '';
+  @Input() type: 'primary' | 'secondary' | 'danger' = 'primary';
+  @Input() disabled: boolean = false;
+}
