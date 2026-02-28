29de69d2fec54725993e3b1b69d804ff4355b781
renananiceto <renan.a.ferreira94@gmail.com>
2026-02-28 02:01:56 +0000

    adiciona components faltantes na home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  ViewChild,
+  AfterViewInit
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardNumberComponent } from '../../shared/components/cardNumber/cardNumber.component';
 import { ButtonComponent } from '../../shared/components/button/button.component'
@@ -9,27 +14,65 @@ import { ButtonComponent } from '../../shared/components/button/button.component
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements AfterViewInit {
+
+  @ViewChild('ctaCard') ctaCard!: ElementRef;
+
+  ngAfterViewInit() {
+    const card = this.ctaCard.nativeElement;
+
+    card.addEventListener('mousemove', (e: MouseEvent) => {
+      const rect = card.getBoundingClientRect();
+
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+
+      card.style.setProperty('--x', `${x}px`);
+      card.style.setProperty('--y', `${y}px`);
+    });
+  }
   categories = [
     {
       icon: 'assets/icons/construction.png',
       title: 'Concursos de Construção',
-      description: 'Publique concursos para obras novas, remodelações e ampliações.'
+      description: 'Publique concursos para obras novas, remodelações, legalizações ou ampliações, e encontre as melhores empresas para o seu projeto.'
     },
     {
       icon: 'assets/icons/services.png',
       title: 'Concursos de Serviços',
-      description: 'Gestão de obra, arquitetura, engenharia e fiscalização.'
+      description: 'Gestão de obra, fiscalização, arquitetura, engenharia, topografia e outros serviços técnicos, tudo com total transparência e controlo.'
     },
     {
       icon: 'assets/icons/subcontract.png',
       title: 'Concursos de Subcontratação',
-      description: 'Encontre subempreiteiros especializados para fases específicas.'
+      description: 'EEncontre subempreiteiros especializados para fases específicas da contrução desde estruturas a acabamentos.'
     },
     {
       icon: 'assets/icons/others.png',
       title: 'Outros',
-      description: 'Organize concursos para aluguer de equipamentos e fornecimento.'
+      description: 'Organize concursos para o aluguer de equipamentos, fornecimento de materiais de construção ou qualquer outra necessidade do setor.'
+    }
+  ];
+  principles = [
+    {
+      icon: 'assets/icons/create.png',
+      title: 'Sem Subscrição',
+      description: 'Pague apenas pelo que utiliza. Sem taxas ocultas, sem encargos recorrentes, sem compromissos'
+    },
+    {
+      icon: 'assets/icons/time.png',
+      title: 'Transparência',
+      description: 'Visibilidade total sobre todos os processos, preços e progresso dos projetos. O que vê é exatamente o que obtém'
+    },
+    {
+      icon: 'assets/icons/trophy.png',
+      title: 'Criar Concursos Grátis',
+      description: 'Inicie o seu concurso sem custos. Obtenha propostas competitivas de empreiteiros qualificados sem taxas iniciais'
+    },
+    {
+      icon: 'assets/icons/chat.png',
+      title: 'Colaboradores',
+      description: 'Adira ao nosso programa de colaboradores e receba uma percentagem dos créditos utilizados pelos convidados que trouxe para a plataforma'
     }
   ];
   steps = [
@@ -55,4 +98,5 @@ export class HomeComponent {
     }
   ];
 }
+
 
